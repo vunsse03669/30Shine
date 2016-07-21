f99a0f51913d738565bd38fcfe9c36884e2d210b
Do Ngoc Trinh <dongoctrinh@Dos-iMac.local>
2016-07-21 18:14:00 +0000

    Detail Salon view create

---
+++ 30Shine/30Shine/DetailSalonView.swift
@@ -0,0 +1,14 @@
+//
+//  DetailSalon.swift
+//  30Shine
+//
+//  Created by Do Ngoc Trinh on 7/21/16.
+//  Copyright © 2016 vu. All rights reserved.
+//
+
+import UIKit
+
+class DetailSalonView: UIView {
+
+
+}
