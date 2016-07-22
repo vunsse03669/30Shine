1259b77ede2f2e1045b9171011ac8ae572030ebf
Do Ngoc Trinh <dongoctrinh@Dos-iMac.local>
2016-07-22 12:53:12 +0000

    [Service screen] init

---
+++ 30Shine/30Shine/OtherServiceCell.swift
@@ -0,0 +1,24 @@
+//
+//  OtherServiceCell.swift
+//  30Shine
+//
+//  Created by Do Ngoc Trinh on 7/22/16.
+//  Copyright © 2016 vu. All rights reserved.
+//
+
+import UIKit
+
+class OtherServiceCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ 30Shine/30Shine/ServiceView.swift
@@ -0,0 +1,21 @@
+//
+//  ServiceView.swift
+//  30Shine
+//
+//  Created by Do Ngoc Trinh on 7/22/16.
+//  Copyright © 2016 vu. All rights reserved.
+//
+
+import UIKit
+
+class ServiceView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
